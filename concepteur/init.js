38b37e2b65c637d6c28cf790181aee6cd6a3b256
nrephi <66361645+nrephi@users.noreply.github.com>
2024-09-02 17:38:55 +0200

    Update init.js

--- concepteur/init.js
+++ concepteur/init.js
@@ -1,7 +1,7 @@
 console.log("loading...")
 
-import "./dist/index.js"
-console.log("loading...")
+require("./dist/index.js")
+
 initConcepteur({ domain: "ebc.plateforme.me" })
 document.getElementById("app").innerHTML = <div>test<PreviewAsync pageId="page_m0kxu9f41b0qmbdq0e4"/></div>
     
