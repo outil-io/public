0172e41b540b48287a2ed58e9aec0ec9a571c148
nrephi <66361645+nrephi@users.noreply.github.com>
2024-09-02 17:10:57 +0200

    Create init.js

---
+++ concepteur/init.js
@@ -0,0 +1,4 @@
+import "./dist/index.js"
+initConcepteur({ domain: "ebc.plateforme.me" })
+document.getElementById("app").innerHTML = <div>test<PreviewAsync pageId="page_m0kxu9f41b0qmbdq0e4"/></div>
+    
